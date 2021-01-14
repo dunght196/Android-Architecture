bc349a0ab714e4ecf0d944bd574c3294a407d5c4
dunght <dunght196@gmail.com>
2021-01-14 23:52:02 +0700

    base adapter 1

--- DaggerTest/app/src/main/java/com/example/daggertest/base/BaseRecyclerViewAdapter.kt
+++ DaggerTest/app/src/main/java/com/example/daggertest/base/BaseRecyclerViewAdapter.kt
@@ -3,17 +3,19 @@ package com.example.daggertest.base
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class BaseRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+abstract class BaseRecyclerViewAdapter<T>(
+    private var items: ArrayList<T> = arrayListOf()
+) : RecyclerView.Adapter<BaseViewHolder<T>>() {
+
+    abstract fun setViewHolder(parent: ViewGroup, viewType: Int) : BaseViewHolder<T>
     
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<T> {
+        return setViewHolder(parent, viewType)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: BaseViewHolder<T>, position: Int) {
+        holder.bind(items[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = items.size
 }
---
+++ DaggerTest/app/src/main/java/com/example/daggertest/base/BaseViewHolder.kt
@@ -0,0 +1,11 @@
+package com.example.daggertest.base
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+
+abstract class BaseViewHolder<T>(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+   LayoutContainer {
+
+   abstract fun bind(item: T)
+}
