cb8fd5424725144cfa07820dc247a00eb3c334e9
dunght <dunght196@gmail.com>
2021-01-17 23:18:41 +0700

    base adapter 2

--- DaggerTest/app/src/main/java/com/example/daggertest/base/BaseRecyclerViewAdapter.kt
+++ DaggerTest/app/src/main/java/com/example/daggertest/base/BaseRecyclerViewAdapter.kt
@@ -1,21 +1,34 @@
 package com.example.daggertest.base
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.daggertest.R
 
 abstract class BaseRecyclerViewAdapter<T>(
-    private var items: ArrayList<T> = arrayListOf()
+    private var listData: ArrayList<T> = arrayListOf()
 ) : RecyclerView.Adapter<BaseViewHolder<T>>() {
 
-    abstract fun setViewHolder(parent: ViewGroup, viewType: Int) : BaseViewHolder<T>
-    
+    abstract fun setViewHolder(view: View): BaseViewHolder<T>
+
+    abstract fun getLayoutId(): Int
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<T> {
-        return setViewHolder(parent, viewType)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(getLayoutId(), parent, false)
+        return setViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder<T>, position: Int) {
-        holder.bind(items[position])
+        holder.bind(listData[position])
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = listData.size
+
+    fun appendData(items: MutableList<T>) {
+        listData.clear()
+        listData.addAll(items)
+        notifyDataSetChanged()
+    }
 }
--- DaggerTest/app/src/main/java/com/example/daggertest/home/ui/MoviePopularAdapter.kt
+++ DaggerTest/app/src/main/java/com/example/daggertest/home/ui/MoviePopularAdapter.kt
@@ -4,43 +4,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
 import com.example.daggertest.R
+import com.example.daggertest.base.BaseRecyclerViewAdapter
+import com.example.daggertest.base.BaseViewHolder
 import com.example.daggertest.network.response.MoviePopularInfo
 
-class MoviePopularAdapter(
-    var listMovies: MutableList<MoviePopularInfo> = mutableListOf()
-) : RecyclerView.Adapter<MoviePopularAdapter.ViewHolder>() {
+class MoviePopularAdapter : BaseRecyclerViewAdapter<MoviePopularInfo>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_movie, parent, false)
-
-        return ViewHolder(view)
+    override fun setViewHolder(view: View): BaseViewHolder<MoviePopularInfo> {
+        return TestViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(listMovies[position])
-    }
-
-    override fun getItemCount(): Int {
-        return listMovies.size
-    }
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class TestViewHolder(containerView: View) : BaseViewHolder<MoviePopularInfo>(containerView) {
         private val title: TextView? = itemView.findViewById(R.id.tv_title)
         private val overview: TextView? = itemView.findViewById(R.id.tv_over_view)
 
-        fun bind(item: MoviePopularInfo?) {
-            title?.text = item?.original_title
-            overview?.text = item?.overview
+        override fun bind(item: MoviePopularInfo) {
+            title?.text = item.original_title
+            overview?.text = item.overview
         }
     }
 
-    fun appendData(items: MutableList<MoviePopularInfo>) {
-        listMovies.clear()
-        listMovies.addAll(items)
-        notifyDataSetChanged()
+    override fun getLayoutId(): Int {
+        return R.layout.item_movie
     }
 
 }
